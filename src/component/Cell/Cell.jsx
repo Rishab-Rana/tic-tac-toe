89db56b37a41d6454620b6b425088f4716894d45
Rishab-Rana <rishabrana228@gmail.com>
2022-02-03 18:14:26 +0530

    created component for board and cells

---
+++ src/component/Cell/Cell.jsx
@@ -0,0 +1,12 @@
+
+import './Cell.css';
+
+export const Cell = () => {
+    return (
+
+        <button className="cell">
+            <span className="cell-content"></span>
+        </button>
+    );
+}
+
---
+++ src/component/board/Board.jsx
@@ -0,0 +1,21 @@
+
+import './Board.css';
+import { Cell } from '../Cell/Cell';
+
+export const Board = () => {
+    return (
+        
+            <div id="board">
+                <Cell />
+                <Cell />
+                <Cell />
+                <Cell />
+                <Cell />
+                <Cell />
+                <Cell />
+                <Cell />
+                <Cell />
+            </div>
+    );
+}
+
