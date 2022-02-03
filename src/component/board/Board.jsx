275a99685f26394b07aa1922ab857278f08b577d
Rishab-Rana <rishabrana228@gmail.com>
2022-02-03 18:59:39 +0530

    Stetting up cell dynamic class

--- src/component/board/Board.jsx
+++ src/component/board/Board.jsx
@@ -6,15 +6,15 @@ export const Board = () => {
     return (
         
             <div id="board">
-                <Cell value="X" />
-                <Cell value="X" />
-                <Cell value="X" />
-                <Cell value="X" />
-                <Cell value="X" />
-                <Cell value="X" />
-                <Cell value="X" />
-                <Cell value="X" />
-                <Cell value="X" />    
+                <Cell value="X" canHighlight={true} />
+                <Cell value="X" canHighlight={true}/>
+                <Cell value="X" canHighlight={true} />
+                <Cell value="O" canHighlight={false} />
+                <Cell value="O" canHighlight={false} />
+                <Cell value="X" canHighlight={false} />
+                <Cell value="O" canHighlight={false} />
+                <Cell value="" canHighlight={false} />
+                <Cell value="" canHighlight={false} />    
             </div>
     );
 }
